db161acbc84060d9ead78d99ae7a5f29603acec4
Tevin Wang <tevin.wang@warriorlife.net>
2020-09-19 22:51:49 -0700

    austen is good

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
+require("dotenv").config()
+
 module.exports = {
     prefix: "-",
-    token: "NzU3MDQ4MzUyMzA2MzY0NDc4.X2auOA.3iyoV5ZDDUSaw-DXT1hr89uYCHc",
+    token: process.env.DISCORD_TOKEN,
 };
+
+//DISCORD_TOKEN=jsadlkfjsalkfjsadlkfjsadlkfjlk234ilk
